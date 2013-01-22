dd25f159bf73754ccf538f6185a30f7e570bf7b9
chiapas@gmail.com <chiapas@gmail.com@6a3187ee-e14c-a898-730b-2d9ab847637b>
2013-01-22 17:05:11 +0000

    7271 Main
    build-release.bat, build-release.py, build-release64.bat and install-vc2010.vcxproj for X64 build.

--- branches/Main/scripts/build-release64.bat
+++ branches/Main/scripts/build-release64.bat
@@ -7,11 +7,11 @@ REM scripts\build-release.bat
 REM You can add Python to PATH if it's not already there
 REM SET PATH=C:\Python;%PATH%
 
-SET VCPATH="%~1\bin\x86_amd64\vcvarsx86_amd64.bat"
+SET VCPATH="%~2\bin\x86_amd64\vcvarsx86_amd64.bat"
 CALL scripts\vc64.bat %VCPATH%
 IF ERRORLEVEL 1 EXIT /B 1
 
 REM add our nasm.exe, mpress.exe and StripReloc.exe to the path
 SET PATH=%CD%\bin;%PATH%
 
-python -u -B scripts\build-release.py %2 %3 %4 %5 %6
+python -u -B scripts\build-release.py %1 %2 %3 %4 %5 %6 %7 %8
