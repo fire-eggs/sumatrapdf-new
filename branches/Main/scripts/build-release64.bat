274ab7745ee3f19e730d2ecbd791a3c00d734057
chiapas@gmail.com <chiapas@gmail.com@6a3187ee-e14c-a898-730b-2d9ab847637b>
2013-01-23 16:48:29 +0000

    7271 Main
    build-release.py + build-release64.bat:
    
    Make sure the value of -vcpath in build-release64.bat passed to build-release.py woun't cause any problem.
    
    The value for vc64.bat to use should not end with backslash \, so even if a user forgets to type backslash, build-release64.bat will add it for us.
    
    The value for build-release.py should not end with backslash neither, otherwise the parameters there will become messed.

--- branches/Main/scripts/build-release64.bat
+++ branches/Main/scripts/build-release64.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SETLOCAL
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM assumes we're being run from top-level directory as:
 REM scripts\build-release.bat
@@ -7,11 +7,47 @@ REM scripts\build-release.bat
 REM You can add Python to PATH if it's not already there
 REM SET PATH=C:\Python;%PATH%
 
-SET VCPATH="%~2\bin\x86_amd64\vcvarsx86_amd64.bat"
+SET var1=%1
+SET var2=%2
+SET var3=%3
+SET var4=%4
+SET var5=%5
+SET var6=%6
+SET var7=%7
+SET var8=%8
+SET var9=%9
+
+SET VCINSTALL=""
+
+FOR %%A IN (var1 var2 var3 var4 var5 var6 var7 var8 var9) DO (
+
+  SET var=!%%A!
+
+  IF "!var!"=="" (
+    @ECHO "Parameter is empty."
+    @ECHO "Set parameter to "test.""
+    SET var="test"
+  )
+
+  IF NOT "!var!"=="test" (
+
+    @ECHO "Parameter is not Empty. Need to check if it ends with \""
+
+    SET varEnd=!var:~-2,1!
+
+    IF !varEnd!==\ (
+      @ECHO "Parameter ends with N\""
+      SET %%A=!var:~0,-2!"
+      SET VCINSTALL=!var!
+    )
+  )
+)
+
+SET VCPATH="!VCINSTALL:"=!\bin\x86_amd64\vcvarsx86_amd64.bat"
 CALL scripts\vc64.bat %VCPATH%
-IF ERRORLEVEL 1 EXIT /B 1
+REM IF ERRORLEVEL 1 EXIT /B 1
 
 REM add our nasm.exe, mpress.exe and StripReloc.exe to the path
 SET PATH=%CD%\bin;%PATH%
 
-python -u -B scripts\build-release.py %1 %2 %3 %4 %5 %6 %7 %8
+python -u -B scripts\build-release.py %var1% %var2% %var3% %var4% %var5% %var6% %var7% %var8% %var9%
