6016b3fc42a00bb8c53390ba703796d601d9c5bd
chiapas <chiapas@gmail.com>
2013-01-25 10:30:36 +0000

    7276 Main
    Reduce codes in build-release.bat and build-release64.bat.
    Make the check: if the version of build-releaseXX.bat is compatible with the flag -platform=XXX

--- scripts/build-release.bat
+++ scripts/build-release.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SETLOCAL
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM assumes we're being run from top-level directory as:
 REM scripts\build-release.bat
@@ -7,10 +7,51 @@ REM scripts\build-release.bat
 REM You can add Python to PATH if it's not already there
 REM SET PATH=C:\Python;%PATH%
 
+SET i=0
+
+FOR %%A IN (%*) DO (
+  SET /a i+=1
+  SET var!i!=%%A
+)
+
+SET VCINSTALL=""
+SET PLATFORM=""
+
+FOR %%A IN (var1 var2 var3 var4 var5 var6 var7 var8 var9) DO (
+
+  SET var=!%%A!
+
+  IF "!var!"=="X64" SET PLATFORM=X64
+
+  IF "!var!"=="" (
+    @ECHO "Parameter is empty."
+    @ECHO "Set parameter to "test.""
+    SET var="test"
+  )
+
+  IF NOT "!var!"=="test" (
+
+    @ECHO "Parameter is not Empty. Need to check if it ends with \""
+
+    SET varEnd=!var:~-2,1!
+
+    IF !varEnd!==\ (
+      @ECHO "Parameter ends with N\""
+      SET %%A=!var:~0,-2!"
+      SET VCINSTALL=!var!
+    )
+  )
+)
+
+IF %PLATFORM%==X64 (
+    @ECHO Target Platform is X64, please use build-release64.bat or remove -platform=X64
+    EXIT /B 1
+)
+
 CALL scripts\vc.bat
 IF ERRORLEVEL 1 EXIT /B 1
 
 REM add our nasm.exe, mpress.exe and StripReloc.exe to the path
 SET PATH=%CD%\bin;%PATH%
 
-python -u -B scripts\build-release.py %1 %2 %3 %4 %5 %6 %7 %8
+python -u -B scripts\build-release.py %var1% %var2% %var3% %var4% %var5% %var6% %var7% %var8% %var9%
--- scripts/build-release.py
+++ scripts/build-release.py
@@ -104,33 +104,35 @@ def sign(file_path, cert_pwd):
   os.chdir(curr_dir)
 
 def main():
-
-  print "%s%s%s" % ("Visual Studio Installation Path = ",vcpath,"\\")
-  print "SVN Revisionsvn =", svn_revision
-  print "Target Platform =", target_platform
-  
-  if build_prerelease:
-    print("Build prerelease")
-  else:
-    print("Not Build prerelease")
-  if build_test_installer:
-    print("Build Test Installer")
-  else:
-    print("Not Build Test")  
-  if build_rel_installer:
-    print("Build Relese Installer")
-  else:
-    print("Not Build Release")
-  if upload:
-    print("Upload")
-  else:
-    print("Not Upload")
-  if upload_tmp:
-    print("Upload Temp")
-  else:
-    print("Not Upload Temp")
-
   global upload
+
+# ====================================================================
+# Should to be deleted
+  # print "%s%s%s" % ("Visual Studio Installation Path = ",vcpath,"\\")
+  # print "SVN Revisionsvn =", svn_revision
+  # print "Target Platform =", target_platform
+  
+  # if build_prerelease:
+    # print("Build prerelease")
+  # else:
+    # print("Not Build prerelease")
+  # if build_test_installer:
+    # print("Build Test Installer")
+  # else:
+    # print("Not Build Test")  
+  # if build_rel_installer:
+    # print("Build Relese Installer")
+  # else:
+    # print("Not Build Release")
+  # if upload:
+    # print("Upload")
+  # else:
+    # print("Not Upload")
+  # if upload_tmp:
+    # print("Upload Temp")
+  # else:
+    # print("Not Upload Temp")
+# ====================================================================  
   
   if len(args) != 0:
     usage()
