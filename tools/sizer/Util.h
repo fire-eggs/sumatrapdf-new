a3955d0fef2f1114422bbf50ac42485f5c03739e
chiapas <chiapas@gmail.com>
2013-02-17 16:14:11 +0000

    7404 No-Panel
    Update: 7387 --> 7404

--- tools/sizer/Util.h
+++ tools/sizer/Util.h
@@ -9,4 +9,17 @@ class IDiaDataSource;
 
 IDiaDataSource *LoadDia();
 
+const char* BStrToString(str::Str<char>& strInOut, BSTR str, char *defString = "", bool stripWhitespace = false);
+
+class StringInterner {
+    dict::MapStrToInt   strToInt;
+    Vec<const char *>   intToStr;
+    int                 nTotalStrings; // just so we know how effective interning is
+
+public:
+    StringInterner() : nTotalStrings(0) {}
+    int             Intern(const char *s);
+    const char *    GetByIndex(int n) const { return intToStr.At(n); }
+};
+
 #endif
