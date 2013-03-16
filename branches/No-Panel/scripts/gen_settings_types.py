4cde686695312e5ef129031e45bf92cd1c672342
chiapas@gmail.com <chiapas@gmail.com@6a3187ee-e14c-a898-730b-2d9ab847637b>
2013-03-16 06:26:42 +0000

    7616 No-Panel
    Update: 7527 --> 7616

--- branches/No-Panel/scripts/gen_settings_types.py
+++ branches/No-Panel/scripts/gen_settings_types.py
@@ -1,75 +1,317 @@
+import types
+from util import gob_varint_encode, gob_uvarint_encode
 
-class Struct(object):
-    def __init__(self, name, fields):
-        self.name = name
-        self.fields = fields
+def is_valid_signed(bits, val):
+    if type(val) not in (types.IntType, types.LongType): return False
+    e = bits - 1
+    min_val = -(2**e)
+    if val < min_val: return False
+    max_val = (2**e)-1
+    if val > max_val: return False
+    return True
 
-        # calculated
-        self.size = None         # size of the structure (size of all fields)
-        self.base_offset = None  # offset from the beginning of top-level struct
-        self.pack_format = "<"   # little endian
-        self.max_type_len = 0    # for better formatting, lenght of longest field type name
-        self.struct_fields = None
+def is_valid_unsigned(bits, val):
+    if type(val) not in (types.IntType, types.LongType): return False
+    if val < 0: return False
+    if val > 2**bits: return False
+    return True
 
-    def get_struct_fields(self):
-        return [f for f in self.fields if f.is_struct()]
+def is_valid_string(val):
+    if val == None: return True
+    return type(val) in (types.StringType, types.UnicodeType)
 
-    def append_pack_format(self, fmt):
-        self.pack_format += fmt
+class Type(object):
+    def __init__(self, def_val):
+        self.c_type_override = None
+        self.val = def_val
+        assert self.is_valid_val(def_val), "%s is not a valid value of %s" % (str(self.val), str(self))
 
-def is_struct_type(typ): return type(typ) == type(Struct(None, None))
+    def c_type(self):
+        if self.c_type_override != None:
+            return self.c_type_override
+        return self.c_type_class
 
-# TODO: also define which python type is allowed for default values
-# (to be used in Field.__init__)
-class TypeDef(object):
-    def __init__(self, c_name, c_size, pack_format):
-        self.c_name = c_name
-        self.c_size = c_size
-        self.pack_format = pack_format
+    def get_typ_enum(self):
+        return self.type_enum
 
-g_types_map = {
-    "bool"   : TypeDef("bool", 2, "h"),
-    "u16"    : TypeDef("uint16_t", 2, "H"),
-    "i32"    : TypeDef("int32_t", 4, "i"),
-    "u32"    : TypeDef("uint32_t", 4, "I"),
-    "color"  : TypeDef("uint32_t", 4, "I"),
-}
+    def is_struct(self):
+        return isinstance(self, Struct)
 
-def verify_type(typ):
-    assert is_struct_type(typ) or typ in g_types_map, "Unknown type %s" % str(typ)
+class Bool(Type):
+    c_type_class = "bool"
+    type_enum = "TYPE_BOOL"
 
-def get_typ_c_name(typ):
-    verify_type(typ)
-    if typ in g_types_map:
-        return g_types_map[typ].c_name
-    if is_struct_type(typ): return "Ptr<%s>" % typ.name
+    def __init__(self, def_val):
+        super(Bool, self).__init__(def_val)
 
-def get_typ_c_size(typ):
-    verify_type(typ)
-    if typ in g_types_map:
-        return g_types_map[typ].c_size
-    if is_struct_type(typ): return 8
+    def is_valid_val(self, val):
+        return val in (True, False)
 
-def get_pack_format(typ):
-    verify_type(typ)
-    if typ in g_types_map:
-        return g_types_map[typ].pack_format
-    if is_struct_type(typ): return "Ixxxx" # 4 bytes offset + 4 padding bytes
+class U16(Type):
+    c_type_class = "uint16_t"
+    type_enum = "TYPE_U16"
+
+    def is_valid_val(self, val):
+        return is_valid_unsigned(16, val)
+
+class I32(Type):
+    c_type_class = "int32_t"
+    type_enum = "TYPE_I32"
+
+    def is_valid_val(self, val):
+        return is_valid_signed(32, val)
+
+class U32(Type):
+    c_type_class = "uint32_t"
+    type_enum = "TYPE_U32"
+
+    def is_valid_val(self, val):
+        return is_valid_unsigned(32, val)
+
+class U64(Type):
+    c_type_class = "uint64_t"
+    type_enum = "TYPE_U64"
+
+    def is_valid_val(self, val):
+        return is_valid_unsigned(64, val)
+
+# behaves like uint32_t, using unique name to signal intent
+class Color(U32):
+    pass
+
+class String(Type):
+    c_type_class = "const char *"
+    type_enum = "TYPE_STR"
+
+    def is_valid_val(self, val):
+        return is_valid_string(val)
+
+class WString(Type):
+    c_type_class = "const WCHAR *"
+    type_enum = "TYPE_WSTR"
+
+    def is_valid_val(self, val):
+        return is_valid_string(val)
+
+class Float(Type):
+    c_type_class = "float"
+    type_enum = "TYPE_FLOAT"
+
+    def is_valid_val(self, val):
+        return type(val) in (types.IntType, types.LongType, types.FloatType)
+
+# When generating C struct definitions we need the structs
+# in the right order (if Bar refers to Foo, Foo must be defined first).
+# This is a list of all structs in the right order
+g_all_structs = []
+
+def serialize_string(val):
+    # empty strings are encoded as 0 (0 length)
+    # non-empty strings are encoded as uvariant(len(s)+1)
+    # (+1 for terminating 0), followed by string data (including terminating 0)
+    if val == None:
+        data = gob_uvarint_encode(0)
+    else:
+        data = gob_uvarint_encode(len(val)+1)
+        data = data + val + chr(0)
+    return data
 
 class Field(object):
-    def __init__(self, name, typ, def_val):
-        verify_type(typ)
+    def __init__(self, name, typ_val):
         self.name = name
-        self.typ = typ
-        self.def_val = def_val
+        self.typ = typ_val
+        if typ_val.is_struct():
+            # TODO: support NULL values for the struct, represented by using
+            # class for typ_val
+            self.val = typ_val
+        else:
+            self.val = typ_val.val
+        self._serialized = None
 
-        # calculated
+    def c_type(self):
+        return self.typ.c_type()
+
+    def is_struct(self):
+        return self.typ.is_struct()
+
+    def is_signed(self):
+        return type(self.typ) == I32
+
+    def is_unsigned(self):
+        return type(self.typ) in (Bool, U16, U32, U64, Color)
+
+    def is_string(self):
+        return type(self.typ) in (String, WString)
+
+    def is_float(self):
+        return type(self.typ) == Float
+
+    def _serialize(self):
+        if self.is_signed():
+            return gob_varint_encode(long(self.val))
+        if self.is_unsigned():
+            return gob_uvarint_encode(long(self.val))
+        if self.is_string():
+            return serialize_string(self.val)
+        # floats are serialied as strings
+        if self.is_float():
+            return serialize_string(str(self.val))
+        if self.is_struct():
+            off = self.val.offset
+            assert type(off) in (types.IntType, types.LongType)
+            return gob_uvarint_encode(off)
+        assert False, "don't know how to serialize %s" % str(self.typ)
+
+    def serialized(self):
+        if self._serialized == None:
+            self._serialized = self._serialize()
+        return self._serialized
+
+# struct is just a base class
+# subclasses should have class instance fields which is a list of tuples:
+# defining name and type of the struct members:
+# fields = [ ("boolField", Bool(True), ("u32Field", U32(32))]
+#
+# TODO: implement struct inheritance i.e. a subclass should inherit all
+# fields from its parent
+class Struct(Type):
+    c_type_class = ""
+    type_enum = "TYPE_STRUCT_PTR"
+    fields = []
+
+    def __init__(self):
+        global g_all_structs
+
+        self.values = []
+        # fields must be a class variable in Struct's subclass
+        for field in self.fields:
+            (name, typ_val) = field
+
+            assert isinstance(name, type(""))
+            assert isinstance(typ_val, Type)
+            self.values.append(Field(name, typ_val))
+
+        cls = self.__class__
+        if cls not in g_all_structs:
+            g_all_structs.append(cls)
+        self.c_type_override = "%s *" % cls.__name__
+
         self.offset = None
 
-    def is_struct(self):   return is_struct_type(self.typ)
-    def c_type(self):      return get_typ_c_name(self.typ)
-    def c_size(self):      return get_typ_c_size(self.typ)
-    def pack_format(self): return get_pack_format(self.typ)
-    def get_struct_def(self):
-        assert self.is_struct()
-        return self.typ
+    def get_max_field_type_len(self):
+        max_len = 0
+        for v in self.values:
+            if len(v.c_type()) > max_len:
+                max_len = len(v.c_type())
+        return max_len + 3
+
+    def is_valid_val(self, val):
+        return issubclass(val, Struct)
+
+    def name(self):
+        return self.__class__.__name__
+
+# TODO: need to supprt more than one C struct field from a single
+# python Type
+class Array(Type):
+    c_type_class = "int32_t"
+    type_enum = "TYPE_ARRAY"
+
+    def __init__(self, typ):
+        assert isinstance(typ, Type)
+        #assert issubclass(typ, Val)
+        self.typ = typ
+
+def serialize(typ, val):
+    assert isinstance(typ, Type)
+    if isinstance(typ, Bool):
+        # TODO: actually serialize the values
+        return ""
+    assert False, "Unkown typ: %s" % str(typ)
+
+def GetAllStructs(): return g_all_structs
+
+class PaddingSettings(Struct):
+    fields =[
+        ("top", U16(2)),
+        ("bottom", U16(2)),
+        ("left", U16(4)),
+        ("right", U16(4)),
+        ("spaceX", U16(4)),
+        ("spaceY", U16(4))
+    ]
+
+class ForwardSearch(Struct):
+    fields = [
+        ("highlightOffset", I32(0)),
+        ("highlightWidth", I32(15)),
+        ("highlightPermanent", I32(0)),
+        ("highlightColor", Color(0x6581FF)),
+        ("enableTeXEnhancements", Bool(False)),
+]
+
+class RectInt(Struct):
+    fields = [
+        ("x", I32(0)),
+        ("y", I32(0)),
+        ("dx", I32(0)),
+        ("dy", I32(0)),
+    ]
+
+class BasicSettings(Struct):
+    fields = [
+        ("globalPrefsOnly", Bool(False)),
+        ("currLanguage", String(None)), # auto-detect
+        ("toolbarVisible", Bool(True)),
+        ("pdfAssociateDontAsk", Bool(False)),
+        ("pdfAssociateDoIt", Bool(False)),
+        ("checkForUpdates", Bool(True)),
+        ("rememberMRUFiles", Bool(True)),
+        # TODO: useSystemColorScheme obsolete by textColor/pageColor ?
+        ("useSystemColorScheme", Bool(False)),
+        ("inverseSearchCmdLine", String(None)),
+        ("versionToSkip", String(None)),
+        ("lastUpdateTime", String(None)),
+        ("defaultDisplayMode", U16(0)),  # DM_AUTOMATIC
+        # -1 == Fit Page
+        ("defaultZoom", Float(-1)),
+        ("windowState", I32(1)), # WIN_STATE_NORMAL
+        ("windowPos", RectInt()),
+        ("tocVisible", Bool(True)),
+        ("favVisible", Bool(False)),
+        ("sidebarDx", I32(0)),
+        ("tocDy", I32(0)),
+        ("showStartPage", Bool(True)),
+        ("openCountWeek", I32(0)),
+        ("lastPrefUpdate", U64(0)),
+    ]
+
+class AdvancedSettings(Struct):
+    fields = [
+        ("traditionalEbookUI", Bool(True)),
+        ("escToExit", Bool(False)),
+        # TODO: different for different document types? For example, ebook
+        # really needs one just for itself
+        ("textColor", Color(0x0)),      # black
+        ("pageColor", Color(0xffffff)), # white
+        ("mainWindowBackground", Color(0xFFF200)),
+        ("pagePadding", PaddingSettings()),
+        ("forwardSearch", ForwardSearch()),
+
+        # TODO: just for testing
+        ("s", String("Hello")),
+        ("defaultZoom", Float(-1)),
+        ("ws", WString("A wide string")),
+    ]
+
+# TODO: merge basic/advanced into one?
+class Settings(Struct):
+    fields = [
+        ("basic", BasicSettings()),
+        ("advanced", AdvancedSettings()),
+        # TODO: just for testing
+        #Array("intArray", I32, [I32("", 1), I32("", 3)]),
+    ]
+
+settings = Settings()
+version = "2.3"
