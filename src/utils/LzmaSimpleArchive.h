a2da4c5ca1d58eb3ce640be93a59ee66c4af6166
chiapas <chiapas@gmail.com>
2013-03-18 18:22:10 +0000

    7657 No-Panel
    Update: 7654 --> 7657

--- src/utils/LzmaSimpleArchive.h
+++ src/utils/LzmaSimpleArchive.h
@@ -14,9 +14,6 @@ struct FileInfo {
     FILETIME        ftModified;
     const char *    name;
     const char *    compressedData;
-
-    // for internal use
-    size_t          off;
 };
 
 // Note: good enough for our purposes, can be expanded when needed
@@ -29,7 +26,9 @@ struct SimpleArchive {
 
 bool   ParseSimpleArchive(const char *archiveHeader, size_t dataLen, SimpleArchive *archiveOut);
 char*  Decompress(const char *in, size_t inSize, size_t *uncompressedSizeOut, Allocator *allocator);
+int    GetIdxFromName(SimpleArchive *archive, const char *name);
 char * GetFileDataByIdx(SimpleArchive *archive, int idx, Allocator *allocator);
+char * GetFileDataByName(SimpleArchive *archive, const char *fileName, Allocator *allocator);
 bool   ExtractFiles(const char *archivePath, const char *dstDir, const char **files, Allocator *allocator=NULL);
 
 }
