a121604042c4b9a776553c53584c99fb423ff277
chiapas <chiapas@gmail.com>
2013-04-02 08:24:46 +0000

    7878 official
    Update: 7855 --> 7878

--- tools/sertxt_test/SettingsTxtSimple.h
+++ tools/sertxt_test/SettingsTxtSimple.h
@@ -6,19 +6,25 @@
 
 namespace sertxt {
 
+struct SimpleXY {
+    int32_t  x;
+    int32_t  y;
+};
+
 struct Simple {
-    const StructMetadata *  def;
-    bool                    bTrue;
-    bool                    bFalse;
-    uint16_t                u16_1;
-    int32_t                 i32_1;
-    uint32_t                u32_1;
-    uint64_t                u64_1;
-    uint32_t                col_1;
-    float                   float_1;
-    const char *            str_1;
-    const char *            str_escape;
-    const WCHAR *           wstr_1;
+    bool           bTrue;
+    bool           bFalse;
+    uint16_t       u16_1;
+    int32_t        i32_1;
+    uint32_t       u32_1;
+    uint64_t       u64_1;
+    uint32_t       col_1;
+    float          float_1;
+    SimpleXY *     xy1;
+    const char *   str_1;
+    const char *   str_escape;
+    SimpleXY *     xy2;
+    const WCHAR *  wstr_1;
 };
 
 Simple *DeserializeSimple(const char *data, size_t dataLen);
