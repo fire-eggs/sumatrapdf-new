97c48548cd42b85e3727ee1de3b4a361c2d48947
chiapas@gmail.com <chiapas@gmail.com@6a3187ee-e14c-a898-730b-2d9ab847637b>
2013-04-10 04:23:59 +0000

    7939 Main
    Update: 7926 --> 7939

--- branches/Main/src/EbookControls.cpp
+++ branches/Main/src/EbookControls.cpp
@@ -3,20 +3,18 @@
 
 #include "BaseUtil.h"
 #include "EbookControls.h"
-
 #include "BitManip.h"
 #include "HtmlFormatter.h"
+#include "MuiEbookPageDef.h"
 #include "resource.h"
-#include "SvgPath.h"
-#include "TxtParser.h"
+#include "WinUtil.h"
 
 #include "DebugLog.h"
 
-static Style *   styleMainWnd = NULL;
-static Style *   stylePage = NULL;
-static Style *   styleStatus = NULL;
-static Style *   styleProgress = NULL;
-static HCURSOR   gCursorHand = NULL;
+static Style *      styleMainWnd = NULL;
+static HCURSOR      gCursorHand = NULL;
+
+static ParsedMui    ebookMuiDef;
 
 #if 0
 static Rect RectForCircle(int x, int y, int r)
@@ -101,293 +99,30 @@ void PageControl::Paint(Graphics *gfx, int offX, int offY)
     gfx->SetClip(&origClipRegion, CombineModeReplace);
 }
 
-static char *gWinDesc = NULL;
-static size_t gWinDescSize;
-static TxtParser *gParser = NULL;
-
-static TxtNode *GetRootArray(TxtParser* parser)
+Control *CreatePageControl(TxtNode *structDef)
 {
-    TxtNode *root = parser->nodes.At(0);
-    CrashIf(!root->IsArray());
-    return root;
-}
+    CrashIf(!structDef->IsStructWithName("EbookPage"));
+    EbookPageDef *def = DeserializeEbookPageDef(structDef);
+    PageControl *c = new PageControl();
+    Style *style = StyleByName(def->style);
+    c->SetStyle(style);
 
-static Vec<TxtNode*> *GetStructsWithName(TxtNode *root, const char *name)
-{
-    size_t nameLen = str::Len(name);
-    CrashIf(!root->IsArray());
-    Vec<TxtNode*> *res = NULL;
-    TxtNode **n;
-    for (n = root->children->IterStart(); n; n = root->children->IterNext()) {
-        TxtNode *node = *n;
-        if (node->IsStructWithName(name, nameLen)) {
-            if (!res)
-                res = new Vec<TxtNode*>();
-            res->Append(node);
-        }
-    }
-    return res;
-}
+    if (def->name)
+        c->SetName(def->name);
 
-float ParseFloat(const char *s)
-{
-    char *end = (char*)s;
-    return (float)strtod(s, &end);
-}
-struct ParsedPadding {
-    int top;
-    int right;
-    int bottom;
-    int left;
-};
-
-// TODO: be more forgiving with whitespace
-// TODO: allow 1 or 2 elements
-static void ParsePadding(const char *s, ParsedPadding& p)
-{
-    str::Parse(s, "%d %d %d %d", &p.top, &p.right, &p.bottom, &p.left);
-}
-
-// TODO: more enums
-static AlignAttr ParseAlignAttr(const char *s)
-{
-    if (str::EqI(s, "center"))
-        return Align_Center;
-    CrashIf(true);
-    return Align_Left;
-}
-
-// TODO: more enums
-static ElAlign ParseElAlign(const char *s)
-{
-    if (str::EqI(s, "center"))
-        return ElAlignCenter;
-    CrashIf(true);
-    return ElAlignLeft;
-}
-
-#if 0
-    FontStyleRegular    = 0,
-    FontStyleBold       = 1,
-    FontStyleItalic     = 2,
-    FontStyleBoldItalic = 3,
-    FontStyleUnderline  = 4,
-    FontStyleStrikeout  = 8
-#endif
-static Gdiplus::FontStyle ParseFontWeight(const char *s)
-{
-    if (str::EqI(s, "regular"))
-        return FontStyleRegular;
-    CrashIf(true);
-    // TODO: more
-    return FontStyleRegular;
-}
-
-static void AddStyleProp(Style *style, TxtNode *prop)
-{
-    ScopedMem<char> tmp(prop->ValDup());
-
-    if (prop->IsTextWithKey("name")) {
-        style->SetName(tmp);
-        return;
-    }
-
-    if (prop->IsTextWithKey("bg_col")) {
-        style->Set(Prop::AllocColorSolid(PropBgColor, tmp));
-        return;
-    }
-
-    if (prop->IsTextWithKey("col")) {
-        style->Set(Prop::AllocColorSolid(PropColor, tmp));
-        return;
-    }
-
-    if (prop->IsTextWithKey("parent")) {
-        Style *parentStyle = StyleByName(tmp);
-        CrashIf(!parentStyle);
-        style->SetInheritsFrom(parentStyle);
-        return;
-    }
-
-    if (prop->IsTextWithKey("border_width")) {
-        style->SetBorderWidth(ParseFloat(tmp));
-        return;
-    }
-
-    if (prop->IsTextWithKey("padding")) {
-        ParsedPadding padding = { 0 };
-        ParsePadding(tmp, padding);
-        style->SetPadding(padding.top, padding.right, padding.bottom, padding.left);
-        return;
-    }
-
-    if (prop->IsTextWithKey("stroke_width")) {
-        style->Set(Prop::AllocWidth(PropStrokeWidth, ParseFloat(tmp)));
-        return;
-    }
-
-    if (prop->IsTextWithKey("fill")) {
-        style->Set(Prop::AllocColorSolid(PropFill, tmp));
-        return;
-    }
-
-    if (prop->IsTextWithKey("vert_align")) {
-        style->Set(Prop::AllocAlign(PropVertAlign, ParseElAlign(tmp)));
-        return;
-    }
-
-    if (prop->IsTextWithKey("text_align")) {
-        style->Set(Prop::AllocTextAlign(ParseAlignAttr(tmp)));
-        return;
-    }
-
-    if (prop->IsTextWithKey("font_size")) {
-        style->Set(Prop::AllocFontSize(ParseFloat(tmp)));
-        return;
-    }
-
-    if (prop->IsTextWithKey("font_weight")) {
-        style->Set(Prop::AllocFontWeight(ParseFontWeight(tmp)));
-        return;
-    }
-
-    CrashIf(true);
-}
-
-static Style* StyleFromStruct(TxtNode* def)
-{
-    CrashIf(!def->IsStructWithName("style"));
-    Style *style = new Style();
-    size_t n = def->children->Count();
-    for (size_t i = 0; i < n; i++) {
-        TxtNode *node = def->children->At(i);
-        CrashIf(!node->IsText());
-        AddStyleProp(style, node);
-    }
-    CacheStyle(style);
-    return style;
-}
-
-static Vec<Style*> *StylesFromStyleStructs(Vec<TxtNode*> *nodes)
-{
-    size_t n = nodes->Count();
-    Vec<Style*> *res = new Vec<Style*>(n);
-    for (size_t i = 0; i < n; i++) {
-        res->Append(StyleFromStruct(nodes->At(i)));
-    }
-    return res;
-}
-
-static void AddButtonVectorProp(ButtonVector *b, TxtNode *prop)
-{
-    ScopedMem<char> tmp(prop->ValDup());
-    if (prop->IsTextWithKey("name")) {
-        b->SetName(tmp);
-        return;
-    }
-
-    if (prop->IsTextWithKey("path")) {
-        GraphicsPath *gp = svg::GraphicsPathFromPathData(tmp);
-        b->SetGraphicsPath(gp);
-        return;
-    }
-
-    if (prop->IsTextWithKey("style_default")) {
-        Style *style = StyleByName(tmp);
-        CrashIf(!style);
-        b->SetDefaultStyle(style);
-        return;
-    }
-
-    if (prop->IsTextWithKey("style_mouse_over")) {
-        Style *style = StyleByName(tmp);
-        CrashIf(!style);
-        b->SetMouseOverStyle(style);
-        return;
-    }
-
-    CrashIf(true);
-}
-
-static ButtonVector* ButtonVectorFromStruct(TxtNode* def)
-{
-    CrashIf(!def->IsStructWithName("ButtonVector"));
-    ButtonVector *b = new ButtonVector();
-    size_t n = def->children->Count();
-    for (size_t i = 0; i < n; i++) {
-        TxtNode *node = def->children->At(i);
-        CrashIf(!node->IsText());
-        AddButtonVectorProp(b, node);
-    }
-    return b;
-}
-
-static Vec<ButtonVector*> *ButtonVectorsFromStyleStructs(Vec<TxtNode*> *nodes)
-{
-    size_t n = nodes->Count();
-    Vec<ButtonVector*> *res = new Vec<ButtonVector*>(n);
-    for (size_t i = 0; i < n; i++) {
-        res->Append(ButtonVectorFromStruct(nodes->At(i)));
-    }
-    return res;
-}
-
-static char *LoadTextResource(int resId, size_t *sizeOut)
-{
-    HRSRC resSrc = FindResource(NULL, MAKEINTRESOURCE(resId), RT_RCDATA);
-    CrashIf(!resSrc);
-    HGLOBAL res = LoadResource(NULL, resSrc);
-    CrashIf(!res);
-    DWORD size = SizeofResource(NULL, resSrc);
-    const char *resData = (const char*)LockResource(res);
-    char *s = str::DupN(resData, size);
-    *sizeOut = size;
-    UnlockResource(res);
-    return s;
-}
-
-static bool LoadAndParseWinDesc()
-{
-    CrashIf(gWinDesc);
-    gWinDesc = LoadTextResource(IDD_EBOOK_WIN_DESC, &gWinDescSize);
-    gParser = new TxtParser();
-    gParser->SetToParse(gWinDesc, gWinDescSize);
-    bool ok = ParseTxt(*gParser);
-    CrashIf(!ok);
-    return ok;
-}
-
-// should only be called once at the end of the program
-extern "C" static void DeleteEbookStyles()
-{
-    delete gParser;
-    free(gWinDesc);
+    FreeEbookPageDef(def);
+    return c;
 }
 
 static void CreateEbookStyles()
 {
     CrashIf(styleMainWnd); // only call me once
 
-    Vec<TxtNode*> *nodes = GetStructsWithName(GetRootArray(gParser), "Style");
-    CrashIf(!nodes);
-
-    Vec<Style*> *styles = StylesFromStyleStructs(nodes);
-
     // TODO: support changing this color to gRenderCache.colorRange[1]
     //       or GetSysColor(COLOR_WINDOW) if gGlobalPrefs.useSysColors
 
     styleMainWnd = StyleByName("styleMainWnd");
     CrashIf(!styleMainWnd);
-    stylePage = StyleByName("stylePage");
-    CrashIf(!stylePage);
-    styleStatus = StyleByName("styleStatus");
-    CrashIf(!styleStatus);
-    styleProgress = StyleByName("styleProgress");
-    CrashIf(!styleProgress);
-
-    delete styles;
-    delete nodes;
-    atexit(DeleteEbookStyles);
 }
 
 static void CreateLayout(EbookControls *ctrls)
@@ -411,57 +146,44 @@ static void CreateLayout(EbookControls *ctrls)
     ctrls->mainWnd->layout = l;
 }
 
-// TODO: create the rest of controls
-static void CreateControls(EbookControls *ctrls)
+static void CreateControls(EbookControls *ctrls, ParsedMui& muiInfo)
 {
-    Vec<TxtNode*> *nodes = GetStructsWithName(GetRootArray(gParser), "ButtonVector");
-    CrashIf(!nodes);
-
-    Vec<ButtonVector*> *vecButtons = ButtonVectorsFromStyleStructs(nodes);
-    for (size_t i = 0; i < vecButtons->Count(); i++) {
-        ButtonVector *b = vecButtons->At(i);
-        if (b->IsNamed("nextButton"))
-            ctrls->next = b;
-        else if (b->IsNamed("prevButton"))
-            ctrls->prev = b;
-        else
-            CrashIf(true);
-    }
+    ctrls->next = FindButtonVectorNamed(ebookMuiDef, "nextButton");
     CrashIf(!ctrls->next);
+    ctrls->prev = FindButtonVectorNamed(ebookMuiDef, "prevButton");
     CrashIf(!ctrls->prev);
-    delete vecButtons;
-    delete nodes;
+    ctrls->status = FindButtonNamed(ebookMuiDef, "statusButton");
+    CrashIf(!ctrls->status);
+    ctrls->progress = FindScrollBarNamed(ebookMuiDef, "progressScrollBar");
+    CrashIf(!ctrls->progress);
+    ctrls->progress->hCursor = gCursorHand;
+    ctrls->page = (PageControl*)FindControlNamed(ebookMuiDef, "page");
+    CrashIf(!ctrls->page);
 }
 
 EbookControls *CreateEbookControls(HWND hwnd)
 {
-    if (!gCursorHand)
+    if (!gCursorHand) {
+        RegisterControlCreatorFor("EbookPage", &CreatePageControl);
         gCursorHand  = LoadCursor(NULL, IDC_HAND);
-
-    if (!gWinDesc) {
-        LoadAndParseWinDesc();
+        char *s = LoadTextResource(IDD_EBOOK_WIN_DESC);
+        MuiFromText(s, ebookMuiDef);
+        free(s);
         CreateEbookStyles();
     }
 
     EbookControls *ctrls = new EbookControls;
 
-    CreateControls(ctrls);
-
-    ctrls->progress = new ScrollBar();
-    ctrls->progress->hCursor = gCursorHand;
-    ctrls->progress->SetStyle(styleProgress);
-
-    ctrls->status = new Button(NULL, styleStatus, styleStatus);
-
-    ctrls->page = new PageControl();
-    ctrls->page->SetStyle(stylePage);
+    CreateControls(ctrls, ebookMuiDef);
 
     ctrls->mainWnd = new HwndWrapper(hwnd);
     ctrls->mainWnd->SetMinSize(Size(320, 200));
     ctrls->mainWnd->SetStyle(styleMainWnd);
 
-    ctrls->mainWnd->AddChild(ctrls->next, ctrls->prev, ctrls->page);
-    ctrls->mainWnd->AddChild(ctrls->progress, ctrls->status);
+    for (size_t i = 0; i < ebookMuiDef.allControls.Count(); i++) {
+        Control *c = ebookMuiDef.allControls.At(i);
+        ctrls->mainWnd->AddChild(c);
+    }
     CreateLayout(ctrls);
     return ctrls;
 }
